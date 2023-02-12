e33725a0592fe1fc4a145ca2aa76cfb99ce7c3e0
PuraPreguica <erickcelestimo@gmail.com>
2023-02-12 16:08:01 -0300

    Criando um banco de dados interno

---
+++ lib/data/database.dart
@@ -0,0 +1,14 @@
+import 'package:futter_projeto/data/task_dao.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+Future<Database> detDatabase() async {
+  final String path = join(await getDatabasesPath(), 'task.db');
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute(TaskDao.tableSql);
+    },
+    version: 1,
+  );
+}
